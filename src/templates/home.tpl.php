ada85cfbd1c4937c1260dde073afc159fe3564eb
Galvinor <art_fortuny@hotmail.com>
2021-10-25 17:19:43 +0200

    V 1.7

--- src/templates/home.tpl.php
+++ src/templates/home.tpl.php
@@ -4,12 +4,12 @@ include 'header.tpl.php';
         </aside>
         <br>
         <section class='home'>
-            <img src="/img/join_the_empire.jpg">
+            <img src="/activitat01/img/join_the_empire.jpg">
             <div class="text">
                 <h2 id='need' >The Empire needs YOU</h2>
                 <h4 id='enlist'>ENLIST TODAY!</h4>
             </div>
-            <img src="/img/empire_needs_you.jpg">
+            <img src="/activitat01/img/empire_needs_you.jpg">
         </section>
     </body>
 </html>
